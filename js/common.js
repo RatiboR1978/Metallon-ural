c8ccce522c92486f7d78a2488aae077448ae1466
RatiboR1978 <klov29@mail.ru>
2017-11-02 09:08:26 +0700

    Сделал слайдер

--- js/common.js
+++ js/common.js
@@ -1,6 +1,12 @@
 $(function () {
 
-
+    $('.slider__list').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    });
 
 
 
