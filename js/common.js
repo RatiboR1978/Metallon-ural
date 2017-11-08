d4179e8238c116661d9bf7c3f9478d57a3d0318f
RatiboR1978 <klov29@mail.ru>
2017-11-08 17:20:53 +0700

    Сделал слайдер лого

--- js/common.js
+++ js/common.js
@@ -18,6 +18,30 @@ $(function () {
         ]
     });
 
+    $('.slider-logos__text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-logos__img'
+    });
+    $('.slider-logos__img').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-logos__text',
+        centerMode: true,
+        focusOnSelect: true
+    });
+
+    $('.slick-slide').click(function () {
+        $('.slick-slide img').removeClass("slider-logos__item");
+        if($(".slick-center")) {
+            $(".slick-center img").addClass("slider-logos__item");
+        }
+    });
+
+
+
 
 
 
