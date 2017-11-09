efa2efbb5c34fbe831f7751a6b97474585ffaec0
RatiboR1978 <klov29@mail.ru>
2017-11-09 11:34:05 +0700

    Начал делать модальные окна

--- js/common.js
+++ js/common.js
@@ -28,6 +28,7 @@ $(function () {
     $('.slider-logos__img').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
+        arrows: false,
         asNavFor: '.slider-logos__text',
         centerMode: true,
         focusOnSelect: true
@@ -41,8 +42,62 @@ $(function () {
     });
 
 
+    $(function(){
+
+        $("#wr-tabs").on("click", ".tab", function(){
+
+            var tabs = $(".tab"),
+                cont = $(".tab-cont");
+
+            tabs.removeClass("catalog__list-item--active");
+            cont.removeClass("catalog__item--active");
+            $(this).addClass("catalog__list-item--active");
+            cont.eq($(this).index()).addClass("catalog__item--active");
+
+            return false;
+        });
+    });
+
+    $('.header-mob_menu a').click(function (e) {
+        e.preventDefault();
+        var currentBlock = $(this).attr('href'),
+            currentBlockOffset = $(currentBlock).offset().top;
+
+        $('html, body').animate({
+            scrollTop: currentBlockOffset - 20
+        },500)
+    });
+
 
 
 
 
 });
+
+ymaps.ready(init);
+var myMap,
+    myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [56.86413757, 60.62005800],
+        zoom: 16
+    });
+
+    myPlacemark = new ymaps.Placemark([56.86413757, 60.62005800], {
+        hintContent: 'Металлон Урал'
+    },
+        {
+        preset: 'islands#redIcon'
+    });
+
+    myMap.controls.remove('typeSelector');
+    myMap.controls.remove('searchControl');
+    myMap.controls.remove('fullscreenControl');
+
+    myMap.behaviors.disable([
+        'scrollZoom'
+    ]);
+
+    myMap.geoObjects.add(myPlacemark);
+}
